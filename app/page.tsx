8f3163501ca314b42a77ae001555eda18ab828a3
Troy Chaplin <troy.chaplin@gmail.com>
2024-12-15 08:52:10 -0500

    change: footer and content

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import { IconCards } from "@/blocks/IconCards";
 import { NewsCards } from "@/blocks/NewsCards";
 import { GetStarted } from "@/blocks/GetStarted";
 import { Header } from "@/components/Header";
-import Image from "next/image";
 import { Testimonials } from "@/blocks/Testimonials";
 
 export default function Home() {
@@ -59,79 +58,10 @@ export default function Home() {
             </Header>
             <NewsCards />
           </Section>
-
-          <h2>Not Wrapped</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-            consequat urna sed euismod lobortis. Aenean iaculis, elit non ornare
-            maximus, ligula mauris ultricies felis, sed vestibulum arcu ipsum
-            vitae elit. Nullam sit amet interdum massa. In hac habitasse platea
-            dictumst. Phasellus pretium augue est, semper fringilla lacus auctor
-            in. Fusce eu lectus lacus. Quisque et magna nec eros commodo
-            tristique sit amet a leo. Donec lacinia libero orci, id ultricies
-            felis dapibus eu.
-          </p>
-
-          <Section as="div">
-            <h2>Inside Simple Section Wrapper</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-              consequat urna sed euismod lobortis. Aenean iaculis, elit non
-              ornare maximus, ligula mauris ultricies felis, sed vestibulum arcu
-              ipsum vitae elit. Nullam sit amet interdum massa. In hac habitasse
-              platea dictumst. Phasellus pretium augue est, semper fringilla
-              lacus auctor in. Fusce eu lectus lacus. Quisque et magna nec eros
-              commodo tristique sit amet a leo. Donec lacinia libero orci, id
-              ultricies felis dapibus eu.
-            </p>
-          </Section>
-
-          <Section as="div">
-            <h2>Inside Constrained Section Wrapper</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-              consequat urna sed euismod lobortis. Aenean iaculis, elit non
-              ornare maximus, ligula mauris ultricies felis, sed vestibulum arcu
-              ipsum vitae elit. Nullam sit amet interdum massa. In hac habitasse
-              platea dictumst. Phasellus pretium augue est, semper fringilla
-              lacus auctor in. Fusce eu lectus lacus. Quisque et magna nec eros
-              commodo tristique sit amet a leo. Donec lacinia libero orci, id
-              ultricies felis dapibus eu.
-            </p>
-          </Section>
-
-          <figure className="ui-figure alignleft size-medium">
-            <Image
-              className=""
-              src="https://picsum.photos/id/20/300/225"
-              alt="Images require alt text"
-              width={300}
-              height={225}
-              decoding="async"
-            />
-          </figure>
-
-          <p>
-            Quisque erat diam, porta quis libero eget, ultrices ornare nisi.
-            Vestibulum accumsan mauris sed elit maximus dignissim. Suspendisse
-            ac facilisis felis. Mauris rhoncus mauris mauris, a porttitor diam
-            pellentesque ut. Sed commodo posuere interdum. Cras aliquet mauris
-            sit amet lacus eleifend, ac maximus sem porta. Curabitur consectetur
-            tortor sapien, in maximus orci elementum.
-          </p>
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-            consequat urna sed euismod lobortis. Aenean iaculis, elit non ornare
-            maximus, ligula mauris ultricies felis, sed vestibulum arcu ipsum
-            vitae elit. Nullam sit amet interdum massa. In hac habitasse platea
-            dictumst. Duis et massa mattis enim sagittis pellentesque. Sed
-            turpis ex, sollicitudin sed est et, pulvinar malesuada nunc. Morbi
-            pulvinar nibh nec lectus ullamcorper, vel semper augue cursus. Duis
-            dapibus nisl eget purus dignissim, vel sollicitudin diam fermentum.
-          </p>
         </Section>
       </Main>
+
+      <footer className="border-b-[80px] idc-footer bg-idc-blue-700 h-[600px] border-idc-blue-900"></footer>
     </>
   );
 }
