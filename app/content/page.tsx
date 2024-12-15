6f4a60b6c1424a6ed79e0158616b3f59cd7b4c48
Troy Chaplin <15524465+troychaplin@users.noreply.github.com>
2024-12-15 08:54:27 -0500

    change: footer and content (#19)

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,15 +1,21 @@
-import Image from "next/image";
 import { Main } from "@/components/Main";
 import { Section } from "@/components/Section";
+import { SiteNav } from "@/blocks/SiteNav";
+import Image from "next/image";
 import { Placeholder } from "@/components/Placeholder";
+import { HeroHomepage } from "@/blocks/HeroHomepage";
 
-export default function Content() {
+export default function Home() {
   return (
     <>
-      <header>Header Row</header>
+      <SiteNav />
 
       <Main>
-        <Section maxWidth="2xl">
+        <Section maxWidth="4xl">
+          <Section as="div" maxWidth="6xl" bgType="gradient">
+            <HeroHomepage />
+          </Section>
+
           <h1>Header One</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
@@ -439,6 +445,77 @@ export default function Content() {
             dapibus lectus, a cursus dui tellus sagittis turpis. Maecenas nec
             metus luctus, tincidunt mauris in, tristique urna. Fusce commodo
             malesuada interdum.
+          </p>
+
+          <h2>Not Wrapped</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            consequat urna sed euismod lobortis. Aenean iaculis, elit non ornare
+            maximus, ligula mauris ultricies felis, sed vestibulum arcu ipsum
+            vitae elit. Nullam sit amet interdum massa. In hac habitasse platea
+            dictumst. Phasellus pretium augue est, semper fringilla lacus auctor
+            in. Fusce eu lectus lacus. Quisque et magna nec eros commodo
+            tristique sit amet a leo. Donec lacinia libero orci, id ultricies
+            felis dapibus eu.
+          </p>
+
+          <Section as="div">
+            <h2>Inside Simple Section Wrapper</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+              consequat urna sed euismod lobortis. Aenean iaculis, elit non
+              ornare maximus, ligula mauris ultricies felis, sed vestibulum arcu
+              ipsum vitae elit. Nullam sit amet interdum massa. In hac habitasse
+              platea dictumst. Phasellus pretium augue est, semper fringilla
+              lacus auctor in. Fusce eu lectus lacus. Quisque et magna nec eros
+              commodo tristique sit amet a leo. Donec lacinia libero orci, id
+              ultricies felis dapibus eu.
+            </p>
+          </Section>
+
+          <Section as="div">
+            <h2>Inside Constrained Section Wrapper</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+              consequat urna sed euismod lobortis. Aenean iaculis, elit non
+              ornare maximus, ligula mauris ultricies felis, sed vestibulum arcu
+              ipsum vitae elit. Nullam sit amet interdum massa. In hac habitasse
+              platea dictumst. Phasellus pretium augue est, semper fringilla
+              lacus auctor in. Fusce eu lectus lacus. Quisque et magna nec eros
+              commodo tristique sit amet a leo. Donec lacinia libero orci, id
+              ultricies felis dapibus eu.
+            </p>
+          </Section>
+
+          <figure className="ui-figure alignleft size-medium">
+            <Image
+              className=""
+              src="https://picsum.photos/id/20/300/225"
+              alt="Images require alt text"
+              width={300}
+              height={225}
+              decoding="async"
+            />
+          </figure>
+
+          <p>
+            Quisque erat diam, porta quis libero eget, ultrices ornare nisi.
+            Vestibulum accumsan mauris sed elit maximus dignissim. Suspendisse
+            ac facilisis felis. Mauris rhoncus mauris mauris, a porttitor diam
+            pellentesque ut. Sed commodo posuere interdum. Cras aliquet mauris
+            sit amet lacus eleifend, ac maximus sem porta. Curabitur consectetur
+            tortor sapien, in maximus orci elementum.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            consequat urna sed euismod lobortis. Aenean iaculis, elit non ornare
+            maximus, ligula mauris ultricies felis, sed vestibulum arcu ipsum
+            vitae elit. Nullam sit amet interdum massa. In hac habitasse platea
+            dictumst. Duis et massa mattis enim sagittis pellentesque. Sed
+            turpis ex, sollicitudin sed est et, pulvinar malesuada nunc. Morbi
+            pulvinar nibh nec lectus ullamcorper, vel semper augue cursus. Duis
+            dapibus nisl eget purus dignissim, vel sollicitudin diam fermentum.
           </p>
         </Section>
       </Main>
